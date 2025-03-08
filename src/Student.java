621b2fb7e517b582d0a694a1022055231fb9873b
Ask-pro <ask.prolevel@gmail.com>
2025-03-09 00:32:03 +0530

    Added house of the student in  Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,10 @@
 public class Student{
     // Method to display details of specific student
-     void displaydetails(String name, int age, int id){
+     void displaydetails(String name, int age, int id, String house){
         System.out.println("Name of the student: " + name);
         System.out.println("Age: " + age);
-        System.out.println("Enrollment Number: " + id + "\n");
+        System.out.println("Enrollment Number: " + id);
+        System.out.println("House Name:" + house + "\n"
      }
 
      // Method to display welcome message
@@ -23,10 +24,10 @@ public class Student{
          student1.displaydetails(); // Calling method to display welcome message
 
         // Calling method to display student details
-         student1.displaydetails("Harry Potter",20,1); //student1
-         student2.displaydetails("Hermoine Granger",19,2); //student2
-         student3.displaydetails("Ron Weasley",19,3); //student3
-         student4.displaydetails("Draco Malfoy",20,4); //student4
-         student5.displaydetails("Neville Longbottom",21,5); //student5
+         student1.displaydetails("Harry Potter",20,1,"Gryffindor"); //student1
+         student2.displaydetails("Hermoine Granger",19,2,"Gryffindor"); //student2
+         student3.displaydetails("Ron Weasley",19,3,"Gryffindor"); //student3
+         student4.displaydetails("Draco Malfoy",20,4,"Slytherin"); //student4
+         student5.displaydetails("Neville Longbottom",21,5,"Gryffindor"); //student5
     }
 }
