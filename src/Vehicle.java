2959e8ff68d26e75e4545b3124387eac77fca49f
Anu <ask.prolevel@gmail.com>
2025-03-16 03:02:33 +0530

    Added and updated projects with latest concepts

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,7 +1,7 @@
 abstract class Vehicle {
-    private String brand; // Instance variable
-    private int year; // Instance variable
-    private String name;
+    private String brand; // Brand (Encapsulation)
+    private int year; // Year (Encapsulation)
+    private String name; // Name (Encapsulation)
 
     void setBrand(String brand) {
         this.brand = brand;
@@ -27,14 +27,14 @@ abstract class Vehicle {
         return year;
     }
 
-    // Display info method
+    // Abstract method (Abstraction)
     abstract void displayinfo();
 
-    // Car class (inherits Vehicle)
+    // Car class (Inheritance)
     static class Car extends Vehicle {
-        private String fuel_type; // Model name
+        private String fuel_type; // Fuel type (Encapsulation)
 
-        // Car constructor
+        // Car constructor (Constructor, Inheritance)
         public Car(String brand, int year, String name, String fuel_type) {
             super.setBrand(brand);
             super.setYear(year);
@@ -42,34 +42,36 @@ abstract class Vehicle {
             this.fuel_type = fuel_type;
         }
 
-
+        // Display method (Polymorphism)
         void displayinfo() {
-            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your car details:"); // Display brand
-            System.out.println("Model name: " +getName()+" - "+fuel_type+"\nManufactured in the year: " + getYear()); // Display car info
+            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your car details:"); // Brand
+            System.out.println("Model name: " +getName()+" - "+fuel_type+"\nManufactured in the year: " + getYear()); // Info
         }
 
-        // Sports class (inherits Car)
+        // Sports class (Inheritance)
         static class Sports extends Car {
-            String speed; // Speed property
+            String speed; // Speed (Encapsulation)
 
-            // Sports constructor
+            // Sports constructor (Constructor, Inheritance)
             public Sports(String sport_name, int sport_year, String sport_model, String sport_type, String speed) {
                 super(sport_name, sport_year, sport_model, sport_type); // Parent constructor
                 this.speed = speed; // Set speed
             }
 
-            // Overriding displayinfo method
+            // Override display (Polymorphism)
             @Override
             void displayinfo() {
-                super.displayinfo(); // Call parent method
-                System.out.print("Speed of the car: " + speed + "\n"); // Display speed info
+                super.displayinfo(); // Parent method
+                System.out.print("Speed of the car: " + speed + "\n"); // Speed
             }
         }
     }
 
+    // Truck class (Inheritance)
     static class Truck extends Vehicle {
-        private String payload;
+        private String payload; // Payload (Encapsulation)
 
+        // Truck constructor (Constructor, Inheritance)
         public Truck(String brand, int year, String name, String payload) {
             super.setBrand(brand);
             super.setYear(year);
@@ -77,15 +79,18 @@ abstract class Vehicle {
             this.payload = payload;
         }
 
+        // Display method (Polymorphism)
         void displayinfo() {
-            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your truck details:"); // Display brand
-            System.out.println("Model name: " +getName()+"\n"+"Manufactured in the year: " + getYear() + "\nPayload: " + payload); // Display car info
+            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your truck details:"); // Brand
+            System.out.println("Model name: " +getName()+"\n"+"Manufactured in the year: " + getYear() + "\nPayload: " + payload); // Info
         }
     }
 
+    // Bike class (Inheritance)
     static class Bike extends Vehicle {
-        private String engine;
+        private String engine; // Engine (Encapsulation)
 
+        // Bike constructor (Constructor, Inheritance)
         public Bike(String brand, int year, String name, String engine) {
             super.setBrand(brand);
             super.setName(name);
@@ -93,52 +98,55 @@ abstract class Vehicle {
             this.engine = engine;
         }
 
+        // Display method (Polymorphism)
         void displayinfo() {
-            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your bike details:"); // Display brand
-            System.out.println("Model name: " +getName()+"\n"+"Manufactured in the year: " + getYear() + "\nEngine type: " + engine); // Display car info
+            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your bike details:"); // Brand
+            System.out.println("Model name: " +getName()+"\n"+"Manufactured in the year: " + getYear() + "\nEngine type: " + engine); // Info
         }
     }
 }
-
 
 // Main class
 class MainClass {
     public static void main(String[] args) {
         // Car array (Object initialization)
-        Vehicle[] vehicle = new Vehicle[3]; // Car array
-        vehicle[0] = new Vehicle.Car("Tata", 2025, "Punch", "EV"); // Create car
-        vehicle[1] = new Vehicle.Car("Renault", 2022, "Duster" ,"Petrol"); // Create car
-        vehicle[2] = new Vehicle.Car("Mahindra", 2023, "Thar" ,"Petrol"); // Create car
+        Vehicle[] vehicle = new Vehicle[3]; // Car array (Polymorphism)
+        vehicle[0] = new Vehicle.Car("Tata", 2025, "Punch", "EV"); // Create car (Object initialization)
+        vehicle[1] = new Vehicle.Car("Renault", 2022, "Duster" ,"Petrol"); // Create car (Object initialization)
+        vehicle[2] = new Vehicle.Car("Mahindra", 2023, "Thar" ,"Petrol"); // Create car (Object initialization)
 
-        // SportsCar array (Object initialization)
-        Vehicle.Car.Sports[] sportcar = new Vehicle.Car.Sports[1];
-        sportcar[0] = new Vehicle.Car.Sports("Porsche",2025,"911","Petrol","330 mph");
+        // SportsCar array (Object initialization, Polymorphism)
+        Vehicle.Car.Sports[] sportcar = new Vehicle.Car.Sports[1]; // SportsCar array
+        sportcar[0] = new Vehicle.Car.Sports("Porsche",2025,"911","Petrol","330 mph"); // Create sports car (Object initialization)
 
-        // Display car info (loop)
+        // Display car info (Loop, Polymorphism)
         for (Vehicle car : vehicle) {
-            car.displayinfo(); // Display car info
+            car.displayinfo(); // Display car info (Polymorphism)
         }
-        // Display sports car info (loop)
+
+        // Display sports car info (Loop, Polymorphism)
         for (Vehicle.Car.Sports sports : sportcar) {
-            sports.displayinfo(); // Display sports info
+            sports.displayinfo(); // Display sports info (Polymorphism)
         }
 
+        // Truck array (Object initialization, Polymorphism)
+        vehicle[0] = new Vehicle.Truck("Tata", 2025, "Ace","10 Ton"); // Create truck
+        vehicle[1] = new Vehicle.Truck("Ashok Leyland", 2022, "Dost", "8 Ton"); // Create truck
+        vehicle[2] = new Vehicle.Truck("Mahindra", 2023, "Bolero","1.5 Ton"); // Create truck
 
-        vehicle[0] = new Vehicle.Truck("Tata", 2025, "Ace","10 Ton"); // Create car
-        vehicle[1] = new Vehicle.Truck("Ashok Leyland", 2022, "Dost", "8 Ton"); // Create car
-        vehicle[2] = new Vehicle.Truck("Mahindra", 2023, "Bolero","1.5 Ton"); // Create car
-
+        // Display truck info (Loop, Polymorphism)
         for(Vehicle truck: vehicle){
-            truck.displayinfo();
+            truck.displayinfo(); // Display truck info (Polymorphism)
         }
 
+        // Bike array (Object initialization, Polymorphism)
+        vehicle[0] = new Vehicle.Bike("Royal Enfield", 2025, "Meteor 350", "Single-Cylinder"); // Create bike
+        vehicle[1] = new Vehicle.Bike("BMW", 2022, "S 1000 RR" ,"Inline Four"); // Create bike
+        vehicle[2] = new Vehicle.Bike("Kawasaki", 2023, "Z900" ,"Parallel Twin"); // Create bike
 
-        vehicle[0] = new Vehicle.Bike("Royal Enfield", 2025, "Meteor 350", "Single-Cylinder"); // Create car
-        vehicle[1] = new Vehicle.Bike("BMW", 2022, "S 1000 RR" ,"Inline Four"); // Create car
-        vehicle[2] = new Vehicle.Bike("Kawasaki", 2023, "Z900" ,"Parallel Twin"); // Create car
-
+        // Display bike info (Loop, Polymorphism)
         for(Vehicle bike: vehicle){
-            bike.displayinfo();
+            bike.displayinfo(); // Display bike info (Polymorphism)
         }
     }
 }
