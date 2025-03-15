06bb5cadd6d6496a36055fe15069b1abdba6d459
Anu <ask.prolevel@gmail.com>
2025-03-16 02:59:20 +0530

    Added few projects and updated existing projects with encapsulation and abstraction concepts

--- src/Students.java
+++ src/Students.java
@@ -1,56 +1,78 @@
-public class Students { // Main class
+abstract class School {
+    private String name;   // Student name
+    private int age;       // Student age
 
-    static class School {
-        String name;   // Student's name
-        int age;       // Student's age
-
-        // Parent constructor: Initializes name and age
-        public School(String studentname, int studentage) {
-            name = studentname; // Initialize name
-            age = studentage;   // Initialize age
-        }
-
-        void displaymessage() {
-            System.out.println("Welcome to Hogwarts!\nHere is the list of student details:\n"); // Welcome message
-        }
-
-        void displayinfo() {
-            System.out.println("Name of the student: " + name); // Print name
-            System.out.println("Age: " + age); // Print age
-        }
+    // Set name
+    public void setName(String name) {
+        this.name = name; // Set name
     }
 
-    static class Student extends School {
-        int id;       // Enrollment ID
-        String house; // House name
-
-        // Subclass constructor: Calls parent constructor and initializes additional properties
-        public Student(String name, int age, int studentid, String studenthouse) {
-            super(name, age); // Call the parent class constructor (School) to set name and age
-            id = studentid;   // Initialize ID specific to Student class
-            house = studenthouse; // Initialize house specific to Student class
-        }
-
-        void displayinfo() { //method overriding as sub class use same method as superclass
-            super.displayinfo();  // Display name and age from parent class, user super since it is method override
-            System.out.println("Enrollment Number: " + id); // Print ID
-            System.out.println("House Name: " + house + "\n"); // Print house
-        }
+    // Get name
+    public String getName() {
+        return name; // Get name
     }
 
+    // Set age
+    public void setAge(int age) {
+        this.age = age; // Set age
+    }
+
+    // Get age
+    public int getAge() {
+        return age; // Get age
+    }
+
+    // Display message
+    void displaymessage() {
+        System.out.println("Welcome to Hogwarts!\nHere is the list of student details:\n"); // Show message
+    }
+
+    // Abstract method
+    abstract void displayinfo(); // Abstract method
+}
+
+class Student extends School {
+    private int id;       // Student ID
+    private String house; // Student house
+
+    // Constructor
+    public Student(String name, int age, int id, String house) {
+        super.setAge(age); // Set age
+        super.setName(name); // Set name
+        this.id = id;   // Set ID
+        this.house = house; // Set house
+    }
+
+    // Display student info
+    void displayinfo() { // Override method
+        System.out.println("Name of the student: " + getName()); // Print name
+        System.out.println("Age: " + getAge()); // Print age
+        System.out.println("Enrollment Number: " + id); // Print ID
+        System.out.println("House Name: " + house + "\n"); // Print house
+    }
+}
+
+class students_main {
     public static void main(String[] args) {
-        Student[] students = new Student[5]; // Array of 5 students
+        // Declare an array of type School instead of Student
+        // This allows us to store objects of type School or any subclass of School (like Student)
+        School[] school = new School[5]; // Array of School type
 
-        students[0] = new Student("Harry Potter", 20, 1, "Gryffindor"); // Student 1
-        students[1] = new Student("Hermione Granger", 19, 2, "Gryffindor"); // Student 2
-        students[2] = new Student("Ron Weasley", 19, 3, "Gryffindor"); // Student 3
-        students[3] = new Student("Draco Malfoy", 17, 4, "Slytherin"); // Student 4
-        students[4] = new Student("Neville Longbottom", 21, 5, "Gryffindor"); // Student 5
+        // Create Student objects and assign them to the School array
+        school[0] = new Student("Hermione", 15, 1, "Gryffindor"); // Student 1
+        school[1] = new Student("Harry", 11, 2, "Gryffindor"); // Student 2
+        school[2] = new Student("Ron", 13, 3, "Gryffindor"); // Student 3
+        school[3] = new Student("Draco", 16, 4, "Slytherin"); // Student 4
+        school[4] = new Student("Neville", 16, 5, "Gryffindor"); // Student 5
 
-        students[0].displaymessage(); // Display welcome message
+        // Show the welcome message for the first student
+        school[0].displaymessage(); // Show message
 
-        for (Student student : students) { // Loop through each student
-            student.displayinfo();  // Display student details
+        /* Loop through each element in the array and call the displayinfo() method
+        Polymorphism: even though the array is of type School, we are calling the displayinfo method
+        of the actual object type (Student). The correct displayinfo() method is called at runtime.*/
+        for (School student : school) { // Loop through array
+            student.displayinfo();  // Polymorphic method call
         }
     }
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,81 +1,144 @@
-public class Vehicle{
-    String brand; // Instance variable
-    int year; // Instance variable
-    String type; // Instance variable
+abstract class Vehicle {
+    private String brand; // Instance variable
+    private int year; // Instance variable
+    private String name;
 
-    // Parent constructor
-    public Vehicle(String brandname, int vehicleyear, String vehicle_type) {
-        brand = brandname; // Set brand
-        year = vehicleyear; // Set year
-        type = vehicle_type; // Set type
+    void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
     }
 
     // Display info method
-    void displayinfo() {
-        System.out.println("Hello! Thanks for your interest in " + brand + ". Below are your car details:"); // Display brand
-    }
+    abstract void displayinfo();
 
     // Car class (inherits Vehicle)
     static class Car extends Vehicle {
-        String carname; // Car name
-        String model; // Model name
+        private String fuel_type; // Model name
 
         // Car constructor
-        public Car(String carname, int caryear, String carmodel, String car_type) {
-            super(carname, caryear, car_type); // Parent constructor
-            model = carmodel; // Set model
+        public Car(String brand, int year, String name, String fuel_type) {
+            super.setBrand(brand);
+            super.setYear(year);
+            super.setName(name);
+            this.fuel_type = fuel_type;
         }
 
-        // Overriding displayinfo method
-        @Override
+
         void displayinfo() {
-            super.displayinfo(); // Call parent method
-            System.out.println("Manufactured in the year: " + year + "\nModel: " + model+"\n"); // Display car info
+            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your car details:"); // Display brand
+            System.out.println("Model name: " +getName()+" - "+fuel_type+"\nManufactured in the year: " + getYear()); // Display car info
         }
 
         // Sports class (inherits Car)
-        static class Sports extends Car{
+        static class Sports extends Car {
             String speed; // Speed property
 
             // Sports constructor
-            public Sports(String sport_name, int sport_year, String sport_model, String sport_type, String sport_speed){
+            public Sports(String sport_name, int sport_year, String sport_model, String sport_type, String speed) {
                 super(sport_name, sport_year, sport_model, sport_type); // Parent constructor
-                speed = sport_speed; // Set speed
+                this.speed = speed; // Set speed
             }
 
             // Overriding displayinfo method
             @Override
             void displayinfo() {
                 super.displayinfo(); // Call parent method
-                System.out.print("Speed of the car: " +speed+ "\n"); // Display speed info
+                System.out.print("Speed of the car: " + speed + "\n"); // Display speed info
             }
         }
     }
+
+    static class Truck extends Vehicle {
+        private String payload;
+
+        public Truck(String brand, int year, String name, String payload) {
+            super.setBrand(brand);
+            super.setYear(year);
+            super.setName(name);
+            this.payload = payload;
+        }
+
+        void displayinfo() {
+            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your truck details:"); // Display brand
+            System.out.println("Model name: " +getName()+"\n"+"Manufactured in the year: " + getYear() + "\nPayload: " + payload); // Display car info
+        }
+    }
+
+    static class Bike extends Vehicle {
+        private String engine;
+
+        public Bike(String brand, int year, String name, String engine) {
+            super.setBrand(brand);
+            super.setName(name);
+            super.setYear(year);
+            this.engine = engine;
+        }
+
+        void displayinfo() {
+            System.out.println("\nHello! Thanks for your interest in " + getBrand()+ ". Below are your bike details:"); // Display brand
+            System.out.println("Model name: " +getName()+"\n"+"Manufactured in the year: " + getYear() + "\nEngine type: " + engine); // Display car info
+        }
+    }
 }
+
 
 // Main class
 class MainClass {
     public static void main(String[] args) {
         // Car array (Object initialization)
-        Vehicle.Car[] cars = new Vehicle.Car[5]; // Car array
-        cars[0] = new Vehicle.Car("Tata", 2025, "Punch", "EV"); // Create car
-        cars[1] = new Vehicle.Car("Renault", 2022, "Duster" ,"Petrol"); // Create car
-        cars[2] = new Vehicle.Car("Mercedes", 2020, "Benz", "EV"); // Create car
-        cars[3] = new Vehicle.Car("Mahindra", 2023, "Thar" ,"Petrol"); // Create car
-        cars[4] = new Vehicle.Car("Tata", 2025, "Nexon" ,"Diesel"); // Create car
+        Vehicle[] vehicle = new Vehicle[3]; // Car array
+        vehicle[0] = new Vehicle.Car("Tata", 2025, "Punch", "EV"); // Create car
+        vehicle[1] = new Vehicle.Car("Renault", 2022, "Duster" ,"Petrol"); // Create car
+        vehicle[2] = new Vehicle.Car("Mahindra", 2023, "Thar" ,"Petrol"); // Create car
 
         // SportsCar array (Object initialization)
         Vehicle.Car.Sports[] sportcar = new Vehicle.Car.Sports[1];
         sportcar[0] = new Vehicle.Car.Sports("Porsche",2025,"911","Petrol","330 mph");
 
         // Display car info (loop)
-        for (Vehicle.Car car : cars) {
+        for (Vehicle car : vehicle) {
             car.displayinfo(); // Display car info
         }
-
         // Display sports car info (loop)
         for (Vehicle.Car.Sports sports : sportcar) {
             sports.displayinfo(); // Display sports info
+        }
+
+
+        vehicle[0] = new Vehicle.Truck("Tata", 2025, "Ace","10 Ton"); // Create car
+        vehicle[1] = new Vehicle.Truck("Ashok Leyland", 2022, "Dost", "8 Ton"); // Create car
+        vehicle[2] = new Vehicle.Truck("Mahindra", 2023, "Bolero","1.5 Ton"); // Create car
+
+        for(Vehicle truck: vehicle){
+            truck.displayinfo();
+        }
+
+
+        vehicle[0] = new Vehicle.Bike("Royal Enfield", 2025, "Meteor 350", "Single-Cylinder"); // Create car
+        vehicle[1] = new Vehicle.Bike("BMW", 2022, "S 1000 RR" ,"Inline Four"); // Create car
+        vehicle[2] = new Vehicle.Bike("Kawasaki", 2023, "Z900" ,"Parallel Twin"); // Create car
+
+        for(Vehicle bike: vehicle){
+            bike.displayinfo();
         }
     }
 }
